a62ec8ebc70be6500d4c8a25d8c4e206ec1c93e5
Rishabh Kohli <kohli.rishiraj@gmail.com>
2020-07-11 00:31:15 +0530

    Updates

---
+++ precache-manifest.c0de90930237976cdf1f8c33de6b5bce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083d9329769d4b9447e3ee55952cc257",
+    "url": "./index.html"
+  },
+  {
+    "revision": "133efc808bd1a0f052ad",
+    "url": "./static/css/main.c66c6255.chunk.css"
+  },
+  {
+    "revision": "fbd2ce0e1e209ddb90a2",
+    "url": "./static/js/2.33377ae2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.33377ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "133efc808bd1a0f052ad",
+    "url": "./static/js/main.6efd76ab.chunk.js"
+  },
+  {
+    "revision": "73c28335255e316cb96d",
+    "url": "./static/js/runtime-main.7b308d91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
